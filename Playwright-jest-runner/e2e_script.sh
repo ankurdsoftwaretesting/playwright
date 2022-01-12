716f3bf04a9df00e8048ddf391648d433ccd93d2
Ankur Dubey <96281960+ankurdsoftwaretesting@users.noreply.github.com>
2022-01-12 16:57:21 +0800

    Create e2e_script.sh

---
+++ Playwright-jest-runner/e2e_script.sh
@@ -0,0 +1,15 @@
+echo "Start test script..."
+
+set -e
+
+docker build --network=host -t test . -f Dockerfile.e2e && docker run -t --ipc=host test
+
+docker container ps -a
+
+echo "copying jest-html-report out of container"
+docker cp $(docker ps -q -l):/usr/src/app/jest-html-report.html . 
+
+echo "copying allure-* out of container"
+docker cp $(docker ps -q -l):/usr/src/app/allure-results .
+
+echo "Finish test."
