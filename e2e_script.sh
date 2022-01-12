a4396f0eb48cb4ed2dab2623b159aa4e3857b5ef
Ankur Dubey <96281960+ankurdsoftwaretesting@users.noreply.github.com>
2022-01-12 22:53:37 +0800

    Update e2e_script.sh

--- e2e_script.sh
+++ e2e_script.sh
@@ -2,7 +2,7 @@ echo "Start test script..."
 
 set -e
 
-docker build -t getting_started . && docker container run --name playJestCon1 getting_started
+docker build -t getting_started . && docker container run --name playJestContainer getting_started
 
 docker container ps -a
 echo "CONTAINER ID ::"
@@ -12,5 +12,17 @@ echo $(docker image ls -q)
 
 echo "copying allure-results out of container"
 docker cp $(docker ps -q -l):/usr/src/app/allure-results .
+
+echo "removing container :: playJestContainer"
+docker container rm playJestContainer
+
+echo "checking ps -a"
+docker container ps -a
+
+echo "removing image :: getting_started"
+docker rmi getting_started
+
+echo "checking images -ls"
+docker images ls
 
 echo "Finish test."
