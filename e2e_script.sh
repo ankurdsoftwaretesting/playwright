03cb69991d5111e2318501161cce7e80c2fde938
Ankur Dubey <96281960+ankurdsoftwaretesting@users.noreply.github.com>
2022-01-13 17:16:50 +0800

    Update e2e_script.sh

--- e2e_script.sh
+++ e2e_script.sh
@@ -8,9 +8,6 @@ echo $(docker ps -q -l)
 echo "IMAGE ID :: " 
 echo $(docker image ls -q)
 
-echo "copying allure-results out of container"
-docker cp $(docker ps -q -l):/usr/src/app/allure-results ./
-
 docker container rm $(docker container ps -a -q)
 docker rmi $(docker images -a -q)
 
