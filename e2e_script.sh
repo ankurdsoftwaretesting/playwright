29640c354c3943f185ae22018c3690cad2342b67
Ankur Dubey <96281960+ankurdsoftwaretesting@users.noreply.github.com>
2022-01-13 14:18:18 +0800

    Update e2e_script.sh

--- e2e_script.sh
+++ e2e_script.sh
@@ -11,18 +11,10 @@ echo $(docker image ls -q)
 echo "copying allure-results out of container"
 docker cp $(docker ps -q -l):/usr/src/app/allure-results .
 
-echo "removing container :: playJestContainer"
-docker container rm playJestContainer
-
 echo "checking ps -a"
 docker container ps -a
 
-echo "removing image :: getting_started"
-docker rmi play-image
-
-docker image prune -a -f
-
-echo "checking images -ls"
+echo "checking images ls"
 docker images ls
 
 echo "Finish test."
