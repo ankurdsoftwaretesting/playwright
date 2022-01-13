a6047825c1d7c63acce395e079abf1abc13a2b0c
Ankur Dubey <96281960+ankurdsoftwaretesting@users.noreply.github.com>
2022-01-13 13:41:51 +0800

    Update e2e_script.sh

--- e2e_script.sh
+++ e2e_script.sh
@@ -1,6 +1,6 @@
 echo "Start test script..."
 
-docker build -t playwrightTestImage . && docker container run --name playJestContainer playwrightTestImage
+docker build -t play-image . && docker container run --name playJestContainer play-image
 
 docker container ps -a
 echo "CONTAINER ID ::"
@@ -18,7 +18,7 @@ echo "checking ps -a"
 docker container ps -a
 
 echo "removing image :: getting_started"
-docker rmi playwrightTestImage
+docker rmi play-image
 
 echo "checking images -ls"
 docker images ls
