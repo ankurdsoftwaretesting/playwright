e1e47d2b741b26ab2da504286da75fed03334ce9
Ankur Dubey <96281960+ankurdsoftwaretesting@users.noreply.github.com>
2022-01-15 21:41:26 +0800

    Update e2e_script.sh

--- e2e_script.sh
+++ e2e_script.sh
@@ -1,6 +1,6 @@
 echo "Start test script..."
 
-docker build --no-cache -t getting-started . && docker container run --name play-jest-container getting-started
+docker build --no-cache -t getting-started . && docker container run --name play-jest-container getting-started -c "npm run test"
 
 docker container ps -a
 echo "CONTAINER ID ::"
